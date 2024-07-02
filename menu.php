11864bb1b869250fd61b51f55bd9bb2c19cdcaf0
Kevin-Mens <kmensah1@umbc.edu>
2024-07-02 14:32:47 -0400

    Quick Fix

--- menu.php
+++ menu.php
@@ -218,7 +218,7 @@
 
 
                 //console.log(.);
-                //window.location.href='battle.php'
+                window.location.href='battle.php'
             }
 
             resize();
